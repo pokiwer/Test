5eeb9b2c415f4af6e6f262ed5d83f050f0012fb4
pokiwer <longcon222001@gmai.com>
2023-03-09 15:11:32 +0700

    test

--- main.js
+++ main.js
@@ -1,22 +1,36 @@
-Array.prototype.myMap = function(cb) {
-    var output = [], len = this.length;
-    for(var i = 0; i < len; ++i)
+Array.prototype.myFilter = function(cb) {
+    var output = [];
+    for(var index in this)
     {
-        var result = cb(this[i],i)
-        output.push(result)
+        if(this.hasOwnProperty(index))
+        {
+            var result = cb(this[index],index,this)
+            if(result)
+            {
+                output.push(this[index]);
+            }
+        }
     }
     return output;
 }
 
 
-const numbers = [1, 2, 3];
 
-console.log(numbers.myMap(function (number) {
-    return number * 2;
-})) // Output: [2, 4, 6]
 
-console.log(numbers.myMap(function (number, index) {
-    return number * index;
-})) // Output: [0, 2, 6]
+
+const numbers = [1, 2, 3, 4];
+
+console.log(numbers.myFilter(function (number) {
+    return number % 2 === 0;
+})); Output: [2, 4]
+
+console.log(numbers.myFilter(function (number, index) {
+    return index % 2 === 0;
+})); Output: [1, 3]
+
+console.log(numbers.myFilter(function (number, index, array) {
+    return array.length % 2 === 0;
+})); Output: [1, 2, 3, 4]
+
 
 

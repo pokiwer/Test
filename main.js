0faf664db62a5b14ed8c7ba13ac71f566a8a51f0
pokiwer <longcon222001@gmai.com>
2023-03-10 14:50:00 +0700

    inner html

--- main.js
+++ main.js
@@ -1,34 +1,10 @@
-Array.prototype.myEvery = function(cb) {
-    var output = true;
-    for(var index in this)
-    {
-        if(this.hasOwnProperty(index))
-        {
-            var results = cb(this[index],index,this)
-            if(!results)
-            { 
-                output = false;
-                break;
-            }
-        }
-    }
-    return output;
+function render(html) {
+    var add = document.querySelector('ul')
+    add.innerHTML = html
 }
 
-
-
-const numbers = [1, 3, 3, 5, 7, 10];
-
-console.log(numbers.myEvery(function (number) {
-    return number % 2 !== 0;
-})); // Output: true
-
-console.log(numbers.myEvery(function (number, index) {
-    return index % 2 === 0;
-})); // Output: false
-
-console.log(numbers.myEvery(function (number, index, array) {
-    return array.length % 2 === 0;
-})); // Output: true
-
-
+render(`
+    <li>Khóa học HTML</li>
+    <li>Khóa học JS</li>
+    <li>Khóa học PHP</li>
+`)

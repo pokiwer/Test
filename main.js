c8426a1896f6c9d8eb007254f73c60d3b4b6fb32
pokiwer <longcon222001@gmai.com>
2023-03-17 16:14:35 +0700

    Create and Delete

--- main.js
+++ main.js
@@ -1,23 +1,106 @@
-var post = 'https://jsonplaceholder.typicode.com/users'
-fetch(post)
-    .then(function(response)
+var post = 'http://localhost:3000/student';
+
+var block = document.querySelector('#block');
+
+//Hàm chạy khi bắt đầu
+function app()
+{
+    getStudents(function(students)
     {
-        return response.json();
-    })
-    .then(function(data)
-    {
-        var html = data.map(function(data)
+        render(students);
+    });
+    createForm();
+}
+
+app();
+
+//Hàm lấy danh sách sinh viên
+function getStudents(callback)
+{
+    fetch(post)
+        .then(function(post)
         {
-            return `<li>
-                <h2>${data.name}</h2>
-                <p>${data.email}</p>
-                <p>${data.address.street}</p>
-            </li>`;
+            return post.json();
         })
-        html = html.join('');
-        document.querySelector('#block').innerHTML = html;
-    })
-    .catch(function()
+        .then(callback)
+}
+//Hàm render lên trình duyệt
+function render(student)
+{
+    var html = student.map(function(data)
     {
-        console.log('Error!')
+        return `<li class = 'student-${data.id}'>
+            <h2>${data.firstName}</h2>
+            <p>
+                ${data.lastName}
+            </p>
+            <button onclick='deleteStudent(${data.id})'>Delete</button>
+        </li>`
     })
+    html = html.join('');
+    block.innerHTML = html;
+}
+
+//Hàm lấy thông tin sinh viên cần thêm
+function createForm()
+{
+    var createBtn = document.querySelector('#create');
+    createBtn.onclick = function()
+    {
+        var firstName = document.querySelector('input[name="firstName"]').value;
+        var lastName = document.querySelector('input[name="lastName"]').value;
+        
+        var form = {
+            firstName : firstName,
+            lastName: lastName
+        }
+        createData(form,function()
+        {
+            getStudents(render)
+        })
+    }
+}
+
+//hàm thêm sinh viên
+function createData(data,callback)
+{
+    var option = 
+    {
+        method : 'post',
+        body : JSON.stringify(data),
+        headers:{
+            'content-Type': 'application/json'
+        },
+    }
+    fetch(post,option)
+        .then(function(reponse)
+        {
+            reponse.json();
+        })
+        .then(callback)
+}
+
+//hàm xóa sinh viên
+function deleteStudent(id)
+{
+    var option =
+    {
+        method: 'delete',
+        headers: {
+        'content-Type':'application/json'
+        }
+    }
+    fetch(post + '/' + id,option)
+        .then(function(reponse)
+        {
+            reponse.json();
+        })
+        .then(function()
+        {
+            var studentItem = document.querySelector('.student-' + id);
+        if(studentItem)
+        {
+            studentItem.remove();
+        }
+        })
+}
